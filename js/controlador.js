8ffb170180225afd04301b9bf8e9fe2d42d49661
rafikis23 <leocasco23@gmail.com>
2021-02-25 17:23:24 -0600

    Tercer  Commit

--- js/controlador.js
+++ js/controlador.js
@@ -46,141 +46,33 @@ var categorias = [];
     }
 
     console.log(categorias);
-    /*
-    var iteracion = categorias.values();
-    for (let valor of iteracion) {
-        console.log(valor.nombreCategoria);
-    }*/
+
 
 })();
 
-// Configurar que guarde en localStorage
-
-// Que se despleguen las categorias
-/*
-var i;
-var j;
-let contador = 0;
-for (i = 0; i < categorias.length; i++) {
-    var elementos = categorias[i];
-    // var constante += categorias[i];
-    //jr.push(elementos.nombreCategoria);
-    // console.log('Las iteraciones', elementos, i);
-    // console.log(elementos.nombreCategoria);
-    // console.log(elementos);
-    for (j = 0; j < elementos.aplicaciones.length; j++) {
-        var salida = elementos.aplicaciones[j];
-        //console.log(salida);
-        generrarApp(salida);
-
-
-
-    };
-*/
-
-/*
-function seleccionarCategoria() {
-    categorias.forEach(function(categoria, i) {
-        seleccionar += `<option value="${i}">${categoria.nombreCategoria}</option>`;
-        // generrarApp(categoria);
-        //categoria.addEventListener('change', generrarApp(categoria));
-
-    });
-    document.getElementById('categoria').innerHTML = seleccionar;
-
-    //generrarApp(categoria.aplicaciones);
-    // document.getElementById('categoria').innerHTML += `<option>${elementos.nombreCategoria}</option>`;
-    // document.addEventListener('DOMContentLoaded', sC);
-    // document.addEventListener('change', elementos.nombreCategoria, true);
-};
-seleccionarCategoria();
-*/
-// <select id="filter-list" name="filter" onchange="filterChanged();"></select>
-// categoria.addEventListener('change', seleccionarCategoria);
-/*
-window.onload = function() {
-    let categoria = document.getElementById('categoria');
-    categoria.onchange = function() {
-        // The code of your function
-        let seleccionar;
-        categorias.forEach(function(categoria, i) {
-            seleccionar += `<option value="${i}">${categoria.nombreCategoria}</option>`;
-            generrarApp(categoria);
-        });
-        categoria.innerHTML = selecionar;
-    }
-}*/
-/*
-function seleccionarCategoria() {
-    let seleccionar;
-    categorias.forEach(function(categoria) {
-        seleccionar += `<option>${categoria.nombreCategoria}</option>`;
-        // generrarApp(categoria);
-    });
-    document.getElementById('categoria').innerHTML = seleccionar;
-
+var localStorage = window.localStorage;
+localStorage.clear();
+for (let i = 0; i < categorias.length; i++) {
+    localStorage.setItem(i, JSON.stringify(categorias[i]));
 }
-document.getElementById('categoria').addEventListener('change', seleccionarCategoria);
-*/
+
+for (let i = 0; i < localStorage.length; i++) {
+    let categoria = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    document.getElementById('categoria').innerHTML +=
+        `<option value="${localStorage.key(i)}">${categoria.nombreCategoria}</option>`;
+}
+
 document.getElementById('categoria').addEventListener('change', seleccionarCategoria());
 
-function seleccionarCategoria(valor) {
-    let seleccionar;
-    categorias.forEach(function(categoria, i) {
-        seleccionar += `<option value=${i}>${categoria.nombreCategoria}</option>`;
-    });
-    document.getElementById('categoria').innerHTML = seleccionar;
-    console.log('Los valores son', categorias[valor].nombreCategoria);
-
-    generrarApp(categorias[valor]);
+function seleccionarCategoria() {
+    document.getElementById('aplicaciones').innerHTML = "";
+    console.log('Categoria seleccionada:', document.getElementById('categoria').value);
+    generrarApp(JSON.parse(localStorage.getItem(document.getElementById('categoria').value)));
 
 }
-// seleccionarCategoria();
 
-/*function start() {
-    document.addEventListener('DOMContentLoaded', sC, true);
-    // document.addEventListener('onchange', sC, false);
-
-}
-start();
-*/
-// generrarApp(categoria);
-
-
-
-
-/*for (let i = 0; i < categorias.length; i++) {
-    const elementos = categorias[i];
-    console.log('Los nombres de la categoria', elementos.nombreCategoria);
-};
-*/
-// document.addEventListener('unload', true);
-
-//document.getElementById('categoria').innerHTML += `<option>${cat.nombreCategoria}</option>`
-
-/*    // document.getElementById('aplicaciones').innerHTML = '';
-    for (let i = 0; i < categorias.length; i++) {
-        var elementos = categorias[i];
-        console.log(elementos.nombreCategoria);
-
-    };
-    for (let j = 0; j < elementos.aplicaciones.length; j++) {
-        var salida = elementos.aplicaciones[j];
-        console.log(salida);
-        // const element = categorias[i];
-        // salida.aplicaciones.forEach(function(app, i) {
-        */
 
 function generrarApp(categoria) {
-    /* for (let i = 0; i < categoria.aplicaciones.length; i++) {
-         let estrellas = '';
-         for (let i = 0; i < categoria.aplicaciones[j].calificacion; i++) {
-             estrellas += '<i class="fas fa-star"></i>';
-         }
-         for (let i = 0; i < 5 - (categoria.aplicaciones[j].calificacion); i++) {
-             estrellas += '<i class="far fa-star" ></i>';
-         }*/
-    //categorias.forEach(function(categoria) {
     let magic;
     categoria.aplicaciones.forEach(function(aplicacion, i) {
 
@@ -192,14 +84,15 @@ function generrarApp(categoria) {
             estrellas += '<i class="far fa-star" ></i>';
         }
         magic +=
-            `<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-                <div class="card">
+            `<div class="col-lg-2 col-md-3 col-6">
+                <div class="card-shadow" onclick="detalleApp(${aplicacion.codigo})">
                     <img src="${aplicacion.icono}" class="card-img-top " alt="... ">
                     <div class="card-body">
                         <h5 class="card-title ">${aplicacion.nombre}</h5>
                         <p class="card-text ">${aplicacion.desarrollador} </p>
                         <div class="my-2">
                             ${estrellas}
+                            <button class="btn btn-outline-danger" onclick="eliminarApp(${i})"><i class="fas fa-trash"></i></button>
                         </div>
                         <div>
                         </div>
@@ -209,9 +102,108 @@ function generrarApp(categoria) {
 
     })
     document.getElementById('aplicaciones').innerHTML = magic;
-
-    // })
 }
-// }
 
-// generrarApp(categoria);
+function eliminarApp(indice) {
+    console.log('La app a eliminar es:', indice);
+}
+
+function detalleApp(codigoAppSeleccionada) {
+    $('#modal-detalle').modal('show');
+    console.log('Categoria seleccionada', document.getElementById('categoria').value);
+    console.log('Codigo de Aplicacion:', codigoAppSeleccionada);
+    let categoria = JSON.parse(localStorage.getItem(document.getElementById('categoria').value));
+    for (let i = 0; i < categoria.aplicaciones.length; i++) {
+        if (codigoAppSeleccionada == categoria.aplicaciones[i].codigo) {
+            let aplicacion = categoria.aplicaciones[i];
+            console.log('La aplicacion a mmostrar en el modal es:');
+            console.log(aplicacion);
+            document.getElementById('nombre-app').innerHTML = aplicacion.nombre;
+            document.getElementById('imagen-app').setAttribute('src', aplicacion.icono);
+            document.getElementById('desarrollador-app').innerHTML = aplicacion.desarrollador;
+            document.getElementById('descripcion-app').innerHTML = aplicacion.descripcion;
+            // document.getElementById('estrellas-app').innerHTML = "";
+            let estrellas = '';
+            for (let i = 0; i < aplicacion.calificacion; i++) {
+                estrellas += '<i class="fas fa-star"></i>';
+            }
+            for (let i = 0; i < 5 - aplicacion.calificacion; i++) {
+                estrellas += '<i class="far fa-star" ></i>';
+            }
+            document.getElementById('estrellas-app').innerHTML = estrellas;
+
+
+
+
+        }
+    }
+
+}
+// Funcion para las opciones de imagenes en una nueva app
+
+function mmostrar() {
+    let imagen;
+    for (let i = 1; i <= 50; i++) {
+        imagen += `<option value="img/app-icons/${i}.webp">${i}.webp</option>`;
+    }
+    console.log('Las aplicaciones', imagen);
+    document.getElementById('icono').innerHTML = imagen;
+    document.getElementById('icono').value = null;
+};
+
+document.getElementById('icono').addEventListener('change', mmostrar());
+
+
+
+/*
+generarImagen((JSON.parse(localStorage.getItem(document.getElementById('categoria').value))));
+
+function generarImagen(categoria) {
+    let imagen;
+    categoria.aplicaciones.forEach(function(app, i) {
+        imagen += `<option value="${app.icono}">${i}.webp</option>`
+    });
+    document.getElementById('icono').innerHTML = imagen;
+}
+// Funcion para visualizar la imagen al momento de una guardar una nueva aplicacion
+document.getElementById('icono').addEventListener('change', visualizarImagen());
+// Para recorrer las imagenes guardadas en localStorage
+
+function visualizarImagen() {
+    if (document.getElementById('icono').value != "")
+        document.getElementById('formImagenApp').setAttribute('src', document.getElementById('icono').value)
+    else
+        document.getElementById('formImagenApp').setAttribute('src', 'img/user.webp');
+}
+
+
+function guardarApp() {
+    let categoria = JSON.parse(localStorage.getItem('categorias'));
+    const app = {
+        nombre: document.getElementById('nombre').value,
+        descripcion: document.getElementById('descripcion').value,
+        icono: document.getElementById('icono').value,
+        calificacion: document.getElementById('calificacion').value,
+        descargas: document.getElementById('descargas').value,
+        desarrollador: document.getElementById('desarrollador').value,
+
+    }
+
+    console.log('Se guardo la aplicacion')
+}
+/* 
+
+function guardarApp() {
+    const app = {
+        nombre: document.getElementById('nombre').value,
+        descripcion: document.getElementById('descripcion').value,
+        icono: document.getElementById('icono').value,
+        calificacion: document.getElementById('calificacion').value,
+        descargas: document.getElementById('descargas').value,
+        desarrollador: document.getElementById('desarrollador').value
+    }
+    console.log(app);
+    $('#modal-nueva-app').modal('hide');
+
+}
+*/
